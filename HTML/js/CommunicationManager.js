6305d849fc69d53bb32be5b8c175a1a1ef527915
Wilson Cheng <chengw01@gmail.com>
2014-12-06 21:54:02 +0000

    RTC work

--- HTML/js/CommunicationManager.js
+++ HTML/js/CommunicationManager.js
@@ -3,36 +3,77 @@ function CommunicationManager(c,u,l,callback,v) {
     this.username = u;
     this.messageSentCallback = callback;
     this.language = l;
+    this.hasVideo = true;
+    this.rtc;
     
     //TIME HACK
     this.videoElement = v;
     
     cm = this; //Pollution
-    
-    var pubNubInit = {
+
+    this.pubNub = PUBNUB.init({
         publish_key: 'pub-c-59a1f5c0-e4a6-48ce-b148-b9b0ca01bb3e',
         subscribe_key: 'sub-c-81c3a310-7d53-11e4-9173-02ee2ddab7fe',
         ssl: true
-    };
+    });
+
+
+    navigator.getMedia = (
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia
+    );
     
-    this.pubNub = PUBNUB.init(pubNubInit);
+    navigator.getMedia({video: true},
+        function(){
+            cm.hasVideo = true;
+            cm.initRTC();
+            
+        },
+        function(){
+            cm.hasVideo = false;
+            cm.initRTC();
+        }
+    );
+
     
-    pubNubInit["number"] = this.username;
     
-    this.rtc = PHONE(pubNubInit);
-    this.rtc.ready = this.dial;
+   // this.rtc.receive = this.receieveCall;
+}
+
+CommunicationManager.prototype.initRTC = function(){
+    //YUCK but it looks like the above modifies the array
+    this.rtc = PHONE({
+        publish_key: 'pub-c-59a1f5c0-e4a6-48ce-b148-b9b0ca01bb3e',
+        subscribe_key: 'sub-c-81c3a310-7d53-11e4-9173-02ee2ddab7fe',
+        ssl: true,
+        media: {audio: true,video:this.hasVideo},
+        number: this.username
+    });
+    this.rtc.ready(function(){
+        console.log("dialing");
+        cm.rtc.dial("Wilson");
+    })
     
-    this.rtc.receive = this.receieveCall;
+    this.rtc.receive(function(session){
+        session.connected(function(session){
+            console.log("call got");
+            document.getElementById("callbox").appendChild(session.video); 
+        });
+    });
 }
 
 CommunicationManager.prototype.receieveCall = function(session){
     session.connected(function(session){
+        console.log("call got");
        PUBNUB.cm.videoElement.appendChild(session.video); 
     });
 }
 
 CommunicationManager.prototype.dial = function() {
-    var session = cm.rtc.dial('Wilson');
+    console.log("dial");
+    var session = cm.rtc.dial('Test2');
 }
 
 CommunicationManager.prototype.startNetwork = function () {
